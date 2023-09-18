36cc24d0c396eac7c6cfdef9e435224d5c854aaf
kennybandy <nekbandy.com>
2023-09-18 17:11:28 +0100

    Prompt the user to input the length if the cube side

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+Surface Area of Cube, A = 6 * (s*s) or A = 6 * (s**2)
+Volume of a cube, V = s**3 or V = s**3 
+*/
+
+//Surface Area of Cube, A = 6 * (s*s) or A = 6 * (s**2)
+
+const cubeSideLength = Number(
+  prompt(
+    "To find the Surface Area of Cube, A, enter the length side of the cube, s"
+  )
+);
